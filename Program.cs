7b489f3d17e046ea0d8f2ccd577ee873cce26fa5
Jussie Segato <jussie.segato@gmail.com>
2023-10-26 20:24:40 -0300

    [Feat] Implementação do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("::::::::Inicio dos Testes com o modelo Nokia::::::");
+Nokia nokia = new("12345", "Modelo X", "ifweokcdsan9909ldasdl", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhastApp");
+Console.WriteLine("::::::::Fim dos Testes com o modelo Nokia::::::");
+
+Console.WriteLine(" ");
+
+Console.WriteLine("::::::::Inicio dos Testes com o modelo Iphone::::::");
+Iphone iphone = new("67890", "Modelo Pro 10", "89pdolap0890kasd0da0", 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhastApp");
+Console.WriteLine("::::::::Fim dos Testes com o modelo Iphone::::::");
